3e4a9f0aa38985a5aef189118c226eba882d5608
Nikola <nikolapacariz@gmail.com>
2018-10-18 15:17:41 +0200

    Vezba br7. -Added action for login user

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,5 +9,15 @@ class LoginController extends Controller
     public function create() {
         return view('Auth.login');
     }
+
+    public function store(Request $request) {
+        $credentials = request()->only(['email', 'password']);
+        if(!auth()->attempt($credentials)) {
+            return redirect()->back();
+        }
+ 
+        return redirect('/home');
+    }
+ 
  
 }
