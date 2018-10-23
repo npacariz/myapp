f1d32be97bd3143317cbc655779f0606465e95eb
Nikola <nikolapacariz@gmail.com>
2018-10-23 10:29:38 +0200

    Removing white spaces from routes

--- routes/api.php
+++ routes/api.php
@@ -12,12 +12,9 @@
  */
 
 Route::group([
-
     'middleware' => 'api',
     'prefix' => 'auth',
-
 ], function ($router) {
-
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
