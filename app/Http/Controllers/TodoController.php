6d59cb02dab9d5f8e7e651f5203c4067eadfad10
Nikola <nikolapacariz@gmail.com>
2018-10-23 11:01:11 +0200

    Adding space in Todo Controller

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Todo;
+use Illuminate\Http\Request;
 
 class TodoController extends Controller {
-    public function index() {
-        return Todo::all();
+
+    /**
+     *  Post method for adding new todos
+     */
+    public function store(Request $request) {
+
     }
 }
